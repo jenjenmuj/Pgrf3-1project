f9127738277bc9628ad7604669e0bc3b1d5fa13d
Honza Vanek <vanek.honza@gmail.com>
2018-02-17 00:31:14 +0100

    Modify the main JOGLApp to scan for samples automatically

--- src/JOGLApp.java
+++ src/JOGLApp.java
@@ -14,6 +14,19 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+
+import java.util.HashMap;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import javax.swing.SwingUtilities;
 
@@ -24,91 +37,148 @@ public class JOGLApp {
 
 	private Frame testFrame;
 	private int demoId = 1;
-	static String[] names = { "lvl1basic.p00.p01withoutShaders", "lvl1basic.p01start.p01buffer",
-			"lvl1basic.p01start.p02attribute", "lvl1basic.p01start.p03uniform", "lvl1basic.p01start.p04utils",
-			"lvl1basic.p01start.p05multiple", "lvl1basic.p01start.p06depthbuffer", "lvl1basic.p02geometry.p01cube",
-			"lvl1basic.p02geometry.p02strip", "lvl1basic.p02geometry.p03obj", "lvl1basic.p03texture.p01intro",
-			"lvl1basic.p03texture.p02utils", "lvl1basic.p04target.p01intro", "lvl1basic.p04target.p02utils",
-			"lvl1basic.p04target.p03postproces", "lvl2advanced.p01run", "lvl2advanced.p02debug",
-			"lvl2advanced.p03texture.p01quad", "lvl2advanced.p03texture.p02cubetexture",
-			"lvl2advanced.p03texture.p03volume", "lvl2advanced.p03texture.p04filtering",
-			"lvl2advanced.p04target.p01save", "lvl2advanced.p04target.p02draw",
-			"lvl2advanced.p04target.p03bufferedimage", "lvl2advanced.p04target.p04multiple",
-			"lvl2advanced.p04target.p05gpgpu", "lvl2advanced.p05pipeline.p01geometryshader",
-			"lvl2advanced.p05pipeline.p02tesselation", "lvl2advanced.p05pipeline.p03query",
-			"lvl2advanced.p06compute.p01intro", "lvl2advanced.p06compute.p02buffer",
-			"lvl2advanced.p06compute.p03texture", "lvl2advanced.p06compute.p04game", "lvl2advanced.p07feedback.p01vs",
-			"lvl2advanced.p07feedback.p02gs", };
-
-	private KeyAdapter keyAdapter;
+	static String[] names = { // edit and comment out the call getDemoNames in start to cherry-pick the samples to be listed
+		"lvl1basic.p00.p01withoutShaders", "lvl1basic.p01start.p01buffer",
+	};
+	static int[] countMenuItems = {15,20};
+	static String[] nameMenuItem = {"basic","advanced"};
+private KeyAdapter keyAdapter;
 
 	public void start() {
 		try {
 			testFrame = new Frame("TestFrame");
 			testFrame.setSize(512, 384);
-
+			
+			System.out.println("searching all samples");
+			
+			getDemoNames("lvl", "\\Renderer.class"); // comment this to have only the samples listed in names
 			makeGUI(testFrame);
 
-			setApp(testFrame, 1);
+			setApp(testFrame, names[0]);
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 	}
+	
+	void getDemoNames(String beginingPath, String nameApp) {
+		Object[] paths;
+		try {
+			paths = Files.find(
+					Paths.get(""),
+					Integer.MAX_VALUE,
+					(filePath, fileAttr) -> (filePath.toString()
+							.lastIndexOf(nameApp)) > 0).toArray();
+			List<String> pathList = new ArrayList<>();
+			for (int i = 1; i < paths.length; i++) {
+				Path path = (Path) paths[i];
+				String s = path.toString();
+				
+				int indexS =  s.indexOf(beginingPath);
+				if (indexS < 0) continue;
+				int indexE =  s.lastIndexOf("\\");
+				if (indexE < 1) continue;
+				
+				s = s.substring(indexS, indexE).trim().replace("\\", ".");
+				pathList.add(s);
+			}
+			Collections.sort(pathList);
 
+			//to find groups of samples
+			HashMap<String, Integer> mapGroups = new HashMap<>();
+			for (int i = 0; i < pathList.size(); i++) {
+				String s = pathList.get(i);
+				System.out.println(s);
+				int index =  s.indexOf(".");
+				if (index < 1) continue;
+				String group = s.substring(0,index).trim();
+				if (mapGroups.containsKey(group))
+					mapGroups.put(group, mapGroups.get(group) + 1);
+				else
+					mapGroups.put(group, 1);
+			}
+
+			names = new String[pathList.size()];
+			int iName = 0;
+			for (Object name : pathList.toArray())
+				names[iName++] = (String) name;
+			
+			countMenuItems = new int[mapGroups.size()];
+			nameMenuItem = new String[mapGroups.size()];
+
+			SortedSet<String> keys = new TreeSet<String>(mapGroups.keySet());
+			int iKey = 0;
+			for (String key : keys) {
+				nameMenuItem[iKey] = key;
+				countMenuItems[iKey] = mapGroups.get(key);
+				// System.out.println(nameMenuItem[iKey]+countMenuItems[iKey]);
+				iKey++;
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	private void makeGUI(Frame testFrame) {
 		ActionListener actionListener = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent ae) {
 				demoId = Integer
 						.valueOf(ae.getActionCommand().substring(0, ae.getActionCommand().lastIndexOf('-') - 1).trim());
-				setApp(testFrame, demoId);
+				setApp(testFrame, names[demoId - 1]);
 			}
 		};
 
 		MenuBar menuBar = new MenuBar();
-		Menu menu = new Menu("Menu");
-		MenuItem m;
-		for (int i = 0; i < names.length; i++) {
-			m = new MenuItem(new Integer(i + 1).toString() + " - " + names[i]);
-			m.addActionListener(actionListener);
-			menu.add(m);
+		int menuIndex = 0;
+		for(int itemMenu = 0 ; itemMenu < nameMenuItem.length; itemMenu++){
+			Menu menu1 = new Menu(nameMenuItem[itemMenu]);
+			MenuItem m;
+			for (int i = 0; i < names.length && i < countMenuItems[itemMenu]; i++) {
+				m = new MenuItem(new Integer(menuIndex + 1).toString() + " - "
+						+ names[menuIndex]);
+				m.addActionListener(actionListener);
+				menu1.add(m);
+				menuIndex++;
+			}
+			menuBar.add(menu1);
 		}
-
+		
 		keyAdapter = new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
 				switch (e.getKeyCode()) {
 				case KeyEvent.VK_HOME:
 					demoId = 1;
-					setApp(testFrame, demoId);
+					setApp(testFrame, names[demoId - 1]);
 
 					break;
 				case KeyEvent.VK_END:
 					demoId = names.length;
-					setApp(testFrame, demoId);
+					setApp(testFrame, names[demoId - 1]);
 					break;
 				case KeyEvent.VK_LEFT:
 					if (demoId > 1)
 						demoId--;
-					setApp(testFrame, demoId);
+					setApp(testFrame, names[demoId - 1]);
 					break;
 				case KeyEvent.VK_RIGHT:
 					if (demoId < names.length)
 						demoId++;
-					setApp(testFrame, demoId);
+					setApp(testFrame, names[demoId - 1]);
 					break;
 				}
 			}
 
 		};
 
-		menuBar.add(menu);
 		testFrame.setMenuBar(menuBar);
 	}
 
-	private void setApp(Frame testFrame, int type) {
+	private void setApp(Frame testFrame, String name) {
 		Dimension dim;
 		if (canvas != null){
 			testFrame.remove(canvas);
@@ -133,7 +203,7 @@ public class JOGLApp {
 		Object ren = null;
 		Class<?> renClass;
 		try {
-			renClass = Class.forName(names[type - 1] + ".Renderer");
+			renClass = Class.forName(name + ".Renderer");
 			ren = renClass.newInstance();
 		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e1) {
 			e1.printStackTrace();
